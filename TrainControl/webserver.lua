e1af7ac86e1441eed7c1c22b2c38f3826c80211c
kovianyo <kovianyo@gmail.com>
2016-10-07 22:12:39 +0200

    pwm start moved up, adding commented out ir

--- TrainControl/webserver.lua
+++ TrainControl/webserver.lua
@@ -5,6 +5,17 @@ wifi.ap.config(cfg)
 
 wifi.setmode(wifi.SOFTAP)
 
+frequency = 1000
+
+pwm.setup(1, frequency, 0)
+pwm.setup(2, frequency, 0)
+
+pwm.start(1)
+pwm.start(2)
+
+irpin1 = 5
+irstop1 = false
+
 html1 =
 [[
 <!DOCTYPE html>
@@ -81,7 +92,7 @@ function setSpeed(speed)
   if speed==0 then
     pwm.setduty(1, 0)
     pwm.setduty(2, 0)
-  elseif speed > 0 then
+  elseif speed > 0 and not irstop1 then
     pwm.setduty(2, 0)
     pwm.setduty(1, speed)
   elseif speed < 0 then
@@ -110,11 +121,14 @@ end)
 
 print("Listening...")
 print("")
+--[[
+function updateIr1()
+    irstop1 = gpio.read(irpin1) == 0
+    if irstop1 then setSpeed(0) end
+end
 
-frequency = 1000
+gpio.mode(irpin1, gpio.INT)
+gpio.trig(irpin1, "both", updateIr1)
 
-pwm.setup(1, frequency, 0)
-pwm.setup(2, frequency, 0)
-
-pwm.start(1)
-pwm.start(2)
+updateIr1()
+--]]
