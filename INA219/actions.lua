a611e7026cdc0b9667c5984c9068d246b3f625f2
kovianyo <kovianyo@gmail.com>
2017-02-15 17:49:09 +0100

    changing to Volt from miliVolt

--- INA219/actions.lua
+++ INA219/actions.lua
@@ -5,7 +5,9 @@ ina_1 = ina219:new()
 ina_1:init(ina_1_adr)
 
 function getVoltage()
-  local voltage = ina_1:read_voltage()
+  local time = tmr.now()
+  local voltage = ina_1:read_voltage() / 1000
+  voltage = round(voltage, 1)
   return voltage
 end
 
