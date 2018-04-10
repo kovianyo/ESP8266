a0df04b0e7ed597710d318563f61384bf888c159
kovianyo <kovianyo@gmail.com>
2018-04-10 07:50:04 +0200

    filtering low outvalues

--- Humidity/actions.lua
+++ Humidity/actions.lua
@@ -4,12 +4,13 @@ function getHumidity()
   local time = tmr.now()
   local H, T = bme280.humi()
 
+  local H2 = bme280.humi()
   -- local voltage = ina219:read_voltage() / 1000
   -- voltage = round(voltage, 2)
 
   log("Elapsed us: ", tmr.now() - time)
 
-  return H
+  return math.max(H, H2)
 end
 
 actions = {
