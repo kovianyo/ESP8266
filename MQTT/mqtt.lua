55fc4065157b6dad361a8b54e45f17674154ce7b
kovianyo <kovianyo@gmail.com>
2019-04-24 18:47:23 +0200

    blinking

--- MQTT/mqtt.lua
+++ MQTT/mqtt.lua
@@ -43,7 +43,11 @@ function setupMqtt()
     print("connected to mqtt broker")
 
     -- subscribe topic with qos = 0
-    local success = client:subscribe(TOPIC, 2, function(client) print("subscribe success") end)
+    local success = client:subscribe(TOPIC, 2,
+     function(client)
+      print("subscribe success")
+      setBlinkLevel(3)
+    end)
     if (not success) then print("subscribe unsuccess ful") end
     -- publish a message with data = hello, QoS = 0, retain = 0
     --client:publish("/topic", "hello", 0, 0, function(client) print("sent") end)
@@ -56,12 +60,53 @@ end
 
 print("wifi status:" .. wifi.sta.status())
 
+
+timer = tmr.create()
+
+onInterval = 1000
+offInterval = 1000
+
+function blink()
+  ledState = not ledState
+  if ledState then
+    gpio.write(ledPin, gpio.HIGH)
+    timer:register(onInterval, tmr.ALARM_SINGLE, function() blink() end)
+    timer:start()
+  else
+    gpio.write(ledPin, gpio.LOW)
+    timer:register(offInterval, tmr.ALARM_SINGLE, function() blink() end)
+    timer:start()
+  end
+end
+
+function setInterval(on, off)
+  if off == nil then off = on end
+  timer:stop()
+  ledState = false
+  onInterval = on
+  offInterval = off
+  blink()
+end
+
+function setBlinkLevel(level)
+  if level == 0 then setInterval(1000)
+  elseif level == 1 then setInterval(500)
+  elseif level == 2 then setInterval(300)
+  else setInterval(1000, 50)
+  end
+end
+
+
+setBlinkLevel(0)
+
 wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
  print("wifi event: Station - CONNECTED. SSID: "..T.SSID..", BSSID: ".. T.BSSID..", Channel: "..T.channel)
+ setBlinkLevel(1)
 end)
 
 wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
  print("wifi event: Station - GOT IP. Station IP: "..T.IP..", Subnet mask: ".. T.netmask..", Gateway IP: "..T.gateway)
+ setBlinkLevel(2)
  setupMqtt()
 end)
 
