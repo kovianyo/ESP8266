bf07314218443e8e500db6806c37171845dc1b2d
kovianyo <kovianyo@gmail.com>
2019-04-27 13:26:09 +0200

    init.lua freeing memory after itself

--- MQTT/init.lua
+++ MQTT/init.lua
@@ -1,12 +1,13 @@
-function checkAbort()
+local checkAbort = function()
   local pin = 0 -- GPIO16, D0
   gpio.mode(pin, gpio.INPUT)
+  print()
   if (gpio.read(pin) == 0) then
-    print('aborting startup')
+    print('Startup aborted.')
     return
   else
-    print('(pull pin ' .. pin .. ' to ground to abort startup)\n')
-    dofile('startup.lua')
+    print('Starting up.. (pull pin ' .. pin .. ' to ground to abort startup)\n')
+    node.task.post(function() dofile('startup.lua') end)
   end
 end
 
